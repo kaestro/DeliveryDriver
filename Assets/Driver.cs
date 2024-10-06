5a694d52602aa37503e1890650661225cd925ced
kaestro <didme07@gmail.com>
2024-10-06 11:41:58 +0900

    adjusted fields

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 0f;
     [SerializeField] float deltaSteer = 0.0001f;
     [SerializeField] float moveSpeed = 0.001f;
     [SerializeField] float deltaMove = 0.0001f;
@@ -29,7 +29,7 @@ public class Driver : MonoBehaviour
     private void HandleSteering()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        steerSpeed += horizontalInput * deltaSteer * Time.deltaTime;
+        steerSpeed -= horizontalInput * deltaSteer * Time.deltaTime;
         transform.Rotate(0, 0, steerSpeed);
     }
 }
