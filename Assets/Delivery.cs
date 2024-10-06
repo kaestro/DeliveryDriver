bab644eb4dad9fd06e431d7e72dfcc3cfd375536
kaestro <didme07@gmail.com>
2024-10-06 15:01:34 +0900

    refactored delivery.cs

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -8,16 +8,26 @@ public class Delivery : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (isDelivering && collision.gameObject.CompareTag("Customer"))
-        {
-            isDelivering = false;
-            Debug.Log("Package delivered!");
-        }
+        HandlePackageDelivery(collision);
 
         Debug.Log("Collision detected with " + collision.gameObject.name);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandlePackagePickup(collision);
+    }
+
+    private void HandlePackageDelivery(Collision2D collision)
+    {
+        if (isDelivering && collision.gameObject.CompareTag("Customer"))
+        {
+            isDelivering = false;
+            Debug.Log("Package delivered!");
+        }
+    }
+
+    private void HandlePackagePickup(Collider2D collision)
     {
         if (collision.CompareTag("Package"))
         {
