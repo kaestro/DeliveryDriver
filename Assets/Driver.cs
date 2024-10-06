c2248820eb19b72cf7b88ab19ec509a53fab5b9e
kaestro <didme07@gmail.com>
2024-10-06 10:57:52 +0900

    implemented handling methods

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 0.01f;
+    [SerializeField] float deltaSteer = 0.0001f;
+    [SerializeField] float moveSpeed = 0.001f;
+    [SerializeField] float deltaMove = 0.0001f;
 
     void Start()
     {
@@ -13,7 +15,21 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, steerSpeed);
+        HandleSteering();
+        HandleMovementSpeed();
+    }
+
+    private void HandleMovementSpeed()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        moveSpeed += verticalInput * deltaMove;
         transform.Translate(0, moveSpeed, 0);
+    }
+
+    private void HandleSteering()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        steerSpeed += horizontalInput * deltaSteer;
+        transform.Rotate(0, 0, steerSpeed);
     }
 }
